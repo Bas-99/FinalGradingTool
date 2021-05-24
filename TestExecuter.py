7faac8d5715359e183505097336d99c911602276
Bas van de Rijt <b.v.d.rijt@student.tue.nl>
2021-05-24 21:12:58 +0200

    TestExecuter.py fully commented

--- TestExecuter.py
+++ TestExecuter.py
@@ -42,20 +42,32 @@ def testExecuter(save_path_filtered, fps, fourcc_avi, folder_dh_DT, assignment, 
     ofsets_h = [431, 431, 266, 1254, 1388]
     ofsets_v = [99, 99, 190, 232, 231]
 
+    # looping over all test functions in test_list
     count = 0
     for current_test in test_list:
+
+        # breaking out of the loop if all tests are computed
         if count >= len(test_names):
             break
 
+        # calling the appRunner function, which will start TwinCat
+        # and Unity
         appRunner(path_unity, path_TwinCat, folder_dh_DT)
 
+        # calling the current_test function to execute the current test
         current_test()
 
+        # calling the screenRec function, to record the initiated test
+        # from the unity environment
         screenRec(fps, sim_time[count], fourcc_avi, names[count],
                   widths[count], heights[count], sizes[count],
                   ofsets_h[count], ofsets_v[count])
 
+        # calling the appCloser function to close both TwinCat and Unity
         appCloser()
+
+        # calling the frameFilter function to resize the videos,
+        # so they take up less disk space
         dir_vid = os.path.join(save_path_filtered, test_names[count])
         frameFilter(names[count], fps, heights[count],
                     widths[count], dir_vid, fourcc_avi, test_names[count], assignment)
